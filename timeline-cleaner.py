564187c1afcae2c0d48b16686878e762618a8122
cmhedrick <christopher.m.hedrick@gmail.com>
2019-08-03 14:01:43 -0400

    completed quick and dirty tl wipe

--- timeline-cleaner.py
+++ timeline-cleaner.py
@@ -16,6 +16,11 @@ class TweetCleaner:
         print('[+] Tweet API Connected')
         return tweepy.API(auth, timeout=120)
 
+    def wipe_timeline(self):
+        for status in tweepy.Cursor(self.api.user_timeline).items():
+            self.api.destroy_status(status.id)
+            print('[+] Del Tweet ID: {}'.format(status.id))
+
 
 if __name__ == "__main__":
     print('[-] Test connection')
@@ -25,3 +30,13 @@ if __name__ == "__main__":
             print('[+] Success')
     except:
         print('[!] FAILED TO CONNECT')
+
+    print('Wipe Timeline?')
+
+    consent = input('[y/n]==> ')
+
+    if consent[0].lower() == 'y':
+        tc.wipe_timeline()
+    else:
+        print('[!] You did not press y')
+        print('[!] No action taken')
